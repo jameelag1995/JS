c5005cfafe5ebc55500501d61e6c235bfbeeaca2
jameelag1995 <agbariajameel@gmail.com>
2023-09-18 17:14:49 +0300

    1.1

---
+++ 1.1/primitive_data_types.js
@@ -0,0 +1,8 @@
+let aString = "Jameel";
+let aNumber = 7;
+let aObject = [1,2,3];
+let aBoolean = true;
+let aNull = null;
+let aUndefined;
+let aSymbol = Symbol("foo");
+console.log(typeof aSymbol);
