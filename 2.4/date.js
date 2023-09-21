349960d739c25cc18f6da89cf286f47efb2f4376
jameelag1995 <agbariajameel@gmail.com>
2023-09-21 10:50:38 +0300

    2.4

---
+++ 2.4/date.js
@@ -0,0 +1,17 @@
+function date() {
+    
+    let todaysDate = new Date();
+  // day of th week
+  let dayOfTheWeek = todaysDate.toLocaleDateString("default", {
+    weekday: "long",
+  });
+  // day of the month
+  let dayOfTheMonth = todaysDate.getDate();
+  // month
+  let month = todaysDate.toLocaleDateString("default", { month: "long" });
+  // year
+  let year = todaysDate.getFullYear();
+
+  return `Today is ${dayOfTheWeek} the ${dayOfTheMonth} of ${month} ${year}`;
+}
+console.log(date());
