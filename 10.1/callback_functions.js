1dff635c37d5d35e05ab89a8b1ea5a07a1a0fc57
jameelag1995 <agbariajameel@gmail.com>
2023-09-26 12:42:23 +0300

    10.1

---
+++ 10.1/callback_functions.js
@@ -0,0 +1,37 @@
+function isString(str,callback){
+    if(typeof str === 'string'){
+        return callback(str);
+    }
+    return -1;
+}
+function printStr(str){
+    console.log(str);
+}
+isString('Hello',printStr);
+
+function firstWordUpperCase(str,callback){
+    const strArr = str.split(' ');
+    strArr[0].toUpperCase();
+    let newStr = callback(strArr);
+    return newStr;
+}
+function dashesBetweenWords(arr){
+    let str = arr.join('-');
+    return str;
+}
+console.log(firstWordUpperCase('hello world wassup!',dashesBetweenWords));
+
+function allWordsUpperCase(str,callback){
+    const strArr = str.split(' ');
+    let newStr = callback(strArr);
+    return newStr;
+}
+function arrOfStringsToUpperCase(arr){
+    const newArr =[]
+    for (let word of arr){
+        newArr.push(word.toUpperCase());
+    }
+    return newArr.join(' ');
+}
+
+console.log(allWordsUpperCase(allWordsUpperCase('hello world wassup!',arrOfStringsToUpperCase),dashesBetweenWords));
