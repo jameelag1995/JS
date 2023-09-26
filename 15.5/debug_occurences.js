95b2f5d8e0e3ecd322dd83df8f415728379d8287
jameelag1995 <agbariajameel@gmail.com>
2023-09-26 15:22:00 +0300

    occurences debug

---
+++ 15.5/debug_occurences.js
@@ -0,0 +1,17 @@
+function countOccurrences(str, char) {
+  let counter = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) === char) {
+      counter++;
+    }
+  }
+  return counter;
+}
+;
+console.log(countOccurrences("ini mini miny moe", "n"))
+/* ----------------------------------- Q1 ----------------------------------- */
+// problem in line 5 counter + 1;
+/* ----------------------------------- Q2 ----------------------------------- */
+// JS REPL
+/* ----------------------------------- Q3 ----------------------------------- */
+// we are not changing the counter and in order to do that  we need to write counter = counter + 1 or counter+=1 or counter++
