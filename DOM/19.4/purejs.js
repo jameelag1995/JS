d14b04d067c23d8227dbbc1199479433f22302e8
jameelag1995 <agbariajameel@gmail.com>
2023-10-02 21:31:14 +0300

    19.4

---
+++ DOM/19.4/purejs.js
@@ -0,0 +1,14 @@
+const h1 = document.createElement('h1');
+h1.innerText = 'Welcome To My Pure JS Website';
+const p = document.createElement('p');
+p.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, eveniet perferendis libero laudantium quasi repellat cupiditate possimus dignissimos et unde?';
+const button = document.createElement('button');
+button.innerText = 'Click here to Win 10M$';
+const header = document.createElement('header');
+header.append(h1,p,button);
+document.body.append(header);
+header.classList.add('myheader');
+h1.classList.add('pad');
+p.classList.add('pad');
+button.classList.add('pad');
+button.classList.add('buttonstyle');
