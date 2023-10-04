031924a60dc8df49dbaef1f10e3d0163957f9951
jameelag1995 <agbariajameel@gmail.com>
2023-10-04 13:51:58 +0300

    22.3

---
+++ DOM/22.3/main.js
@@ -0,0 +1,40 @@
+// Random letter Generator
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+// display user input
+const letter = document.querySelector(".letter");
+const letterDisplay = document.createElement("h2");
+const userinput = document.querySelector("#userinput");
+const display = document.querySelector(".display");
+const displayContent = document.createElement("h3");
+letterDisplay.innerText = "?";
+console.log(randomLetter);
+letter.append(letterDisplay);
+display.append(displayContent);
+
+// Winning PopUp
+const correctMsg = document.createElement("h2");
+correctMsg.innerText = "That Was Correct. Nice Guess!";
+const playagainbtn = document.createElement("button");
+playagainbtn.innerText = "Play Again";
+playagainbtn.classList.add("btn");
+const correctdiv = document.querySelector(".correct");
+correctdiv.append(correctMsg, playagainbtn);
+correctdiv.classList.toggle("correcthidden");
+userinput.focus();
+
+// play again event
+playagainbtn.addEventListener("click", () => {
+    window.location.reload();
+});
+
+// guess the letter event
+userinput.addEventListener("keyup", (e) => {
+    displayContent.innerText += `${e.target.value[e.target.value.length - 1]},`;
+    if (e.target.value[e.target.value.length - 1] === randomLetter) {
+        letterDisplay.innerText = randomLetter;
+        correctdiv.classList.toggle("correcthidden");
+        return;
+    }
+});
