bcf88ff39c72281ca955a4a34b6543095ff232de
jameel Agbaria <agbariajameel@gmail.com>
2024-07-06 11:58:40 +0300

    update class color

--- OOP/Color_Class/colorClass.js
+++ OOP/Color_Class/colorClass.js
@@ -5,26 +5,30 @@ class Color {
         this.b = b;
         this.colorName = name;
     }
-    rgb(){
-        return `rgb(${this.r}, ${this.g}, ${this.b})`;
-    };
-    hex(){
-        return `#${this.r.toString(16)}${this.g.toString(16)}${this.b.toString(16)}`;
+    innerRgb() {
+        return `${this.r}, ${this.g}, ${this.b}`;
     }
-    rgba(a=1.0){
-        return `rgba(${this.r}, ${this.g}, ${this.b}, ${a})`;
+    rgb() {
+        return `rgb(${this.innerRgb()})`;
     }
-    
+    hex() {
+        return `#${this.r.toString(16)}${this.g.toString(16)}${this.b.toString(
+            16
+        )}`;
+    }
+    rgba(a = 1.0) {
+        return `rgba(${this.innerRgb()}, ${a})`;
+    }
 }
-const white = new Color(255,255,255,'white')
+const white = new Color(255, 255, 255, "white");
 console.log(white.colorName);
 console.log(white.rgb());
 console.log(white.hex());
-const red = new Color(255,0,0,'red');
+const red = new Color(255, 0, 0, "red");
 console.log(red.colorName);
 console.log(red.rgb());
 console.log(red.hex());
-const blue = new Color(0,0,255,'blue');
+const blue = new Color(0, 0, 255, "blue");
 console.log(blue.colorName);
 console.log(blue.rgb());
 console.log(blue.hex());
